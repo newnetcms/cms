d9492f5c2bba9561d9d4fef9602de64699e1de5b
Newnet Dev <dev@newnet.vn>
2025-08-30 12:10:31 +0700

    optimize seo url friendly

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Newnet\Cms\Http\Controllers\Web\PageController;
 use Newnet\Cms\Http\Controllers\Web\PostController;
 use Newnet\Cms\Http\Controllers\Web\CategoryController;
 
-Route::get('cms/page/{id}', [PageController::class, 'detail'])->name('cms.web.page.detail');
-Route::get('cms/post/{id}', [PostController::class, 'detail'])->name('cms.web.post.detail');
-Route::get('cms/category/{id}', [CategoryController::class, 'detail'])->name('cms.web.category.detail');
+Route::middleware(['seo.internal.access'])
+    ->group(function () {
+        Route::get('cms/page/{id}', [PageController::class, 'detail'])->name('cms.web.page.detail');
+        Route::get('cms/post/{id}', [PostController::class, 'detail'])->name('cms.web.post.detail');
+        Route::get('cms/category/{id}', [CategoryController::class, 'detail'])->name('cms.web.category.detail');
+    });
